467198ae3b109f639cb2fa37d3b6e3155e9fe6c7
Udo Kohlmeyer <ukohlmeyer@pivotal.io>
2022-06-15 15:51:14 +1000

    Initial Spring Session Data GemFire commit

---
+++ scripts/copySSDGTestResources.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+#if anything errors, bail.
+set -e
+
+ssdgPath="spring-session-data-geode"
+
+Clone() {
+  # Clone SDG repo
+  rm -rf "$ssdgPath"
+  git clone https://github.com/spring-projects/spring-session-data-geode.git $ssdgPath
+}
+
+Checkout() {
+  # Checkout correct branch
+  cd "$ssdgPath" || exit
+  git checkout $branch
+}
+
+CopyTestResources() {
+  rm -rf $projectDir"/src/ssdg-test-read-only"
+  mkdir -p $projectDir"/src"
+  cp -R $ssdgPath"/spring-session-data-geode/src/test/" $projectDir"/src/ssdg-test-read-only"
+  cp -R $ssdgPath"/spring-session-data-geode/src/integration-test/" $projectDir"/src/ssdg-integration-test-read-only"
+}
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+  -l)
+    ssdgPath="$2"
+    shift # past argument
+    shift # past value
+    ;;
+  -b)
+    branch="$2"
+    shift # past argument
+    shift # past value
+    ;;
+  -t)
+    projectDir="$2"
+    shift # past argument
+    shift # past value
+    ;;
+  -h)
+    Help
+    exit
+    ;;
+  -* | --*)
+    echo "Unknown option $1"
+    exit 1
+    ;;
+  esac
+done
+
+Clone
+Checkout
+CopyTestResources
